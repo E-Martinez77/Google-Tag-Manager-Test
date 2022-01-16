91bb086c51893c3aa97f2a3d240bf7f46cb5b115
E-Martinez77 <emartizzle2185@me.com>
2022-01-16 16:37:17 -0700

    hard coded data layer for add to carts added to test in GTM

---
+++ assets/home.js
@@ -0,0 +1,16 @@
+// const atcPush = () => {};
+
+// const atcBtn = document.querySelector("#atc");
+
+// atcBtn.addEventListener("click", () => {
+//   dataLayer.push({
+//     "Order Value": "1.00",
+//     Currency: "USD",
+//     "Order Quantity": "5",
+//     "Order ID": "0-000001",
+//     "Product ID": "0-000001",
+//     "Product Category": "Test_Cat",
+//   });
+
+//   console.log("Push Event - Add To Cart");
+// });
