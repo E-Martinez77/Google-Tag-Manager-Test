ee111d681c0625cb71c5bb81720fbeec1d6e4f58
E-Martinez77 <emartizzle2185@me.com>
2022-04-18 17:42:45 -0600

    added line items for checkout event

--- assets/checkout.js
+++ assets/checkout.js
@@ -1,6 +1,32 @@
 function gtmEvents() {
   dataLayer.push({
     event: "purchase",
+    eventModel: {
+      value: 20,
+      currency: "USD",
+      quantity: 4,
+      orderId: "3234513",
+      items: [
+        {
+          product_id: "434fkdsn4",
+          product_category: "All Products",
+          product_price: 5.43,
+          product_quantity: 5,
+        },
+        {
+          product_id: "55555555523",
+          product_category: "All Products",
+          product_price: 7.1,
+          product_quantity: 5,
+        },
+        {
+          product_id: "0011-2352",
+          product_category: "All Products",
+          product_price: 10.0,
+          product_quantity: 1,
+        },
+      ],
+    },
   });
   console.log("GTM Events Loaded.");
 }
